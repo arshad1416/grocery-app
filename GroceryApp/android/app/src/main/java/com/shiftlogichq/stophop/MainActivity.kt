b8a8c295ac4e30be0185dd34f2c3d3ce8d5cd83d
Arshad <arshad@tagstudios.io>
2026-06-12 17:29:55 -0400

    fix EAS build: add babel.config.js, fix android package name

--- GroceryApp/android/app/src/main/java/com/shiftlogichq/stophop/MainActivity.kt
+++ GroceryApp/android/app/src/main/java/com/shiftlogichq/stophop/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.anonymous.GroceryApp
+package com.shiftlogichq.stophop
 
 import android.os.Build
 import android.os.Bundle
--- GroceryApp/android/app/src/main/java/com/shiftlogichq/stophop/MainApplication.kt
+++ GroceryApp/android/app/src/main/java/com/shiftlogichq/stophop/MainApplication.kt
@@ -1,4 +1,4 @@
-package com.anonymous.GroceryApp
+package com.shiftlogichq.stophop
 
 import android.app.Application
 import android.content.res.Configuration
