3fd334321df568ecfe527cf1c518b88b6b33e6b4
Arshad <arshad@tagstudios.io>
2026-06-12 22:06:52 -0400

    fix: add loose class-properties plugins for WatermelonDB decorator compatibility (Fable finding)
    WatermelonDB @field decorators get destroyed by babel's spec-compliant 'define'
    semantics. Added @babel/plugin-transform-class-properties + private-methods +
    private-property-in-object with loose:true in correct order (decorators first).

--- GroceryApp/babel.config.js
+++ GroceryApp/babel.config.js
@@ -4,6 +4,9 @@ module.exports = function (api) {
     presets: ['babel-preset-expo'],
     plugins: [
       ['@babel/plugin-proposal-decorators', { legacy: true }],
+      ['@babel/plugin-transform-class-properties', { loose: true }],
+      ['@babel/plugin-transform-private-methods', { loose: true }],
+      ['@babel/plugin-transform-private-property-in-object', { loose: true }],
     ],
   };
 };
